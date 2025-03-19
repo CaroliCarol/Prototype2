47b8a22cfeb6b743e5d22dba8fae1513be97fd88
CaroliCarol <a59626@alunos.ipb.pt>
2025-03-19 17:40:16 +0000

    4. Randomize the spawn location

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[ ] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +20,10 @@ public class SpawnManager : MonoBehaviour
     {
       if (Input.GetKeyDown(KeyCode.S))
       {
+        //Randomly generates animal index and spawn position
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Instantiate(animalPrefabs[animalIndex], new Vector3(0,0,20), animalPrefabs[animalIndex].transform.rotation);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
       }
     }
 }
